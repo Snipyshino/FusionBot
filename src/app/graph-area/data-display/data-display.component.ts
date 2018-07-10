a10015e9de8851904420c77a2bf621833dee8557
Shane Stott <shane_stott@intergral.com>
2018-07-10 14:44:39 +0200

    colours

--- src/app/graph-area/data-display/data-display.component.ts
+++ src/app/graph-area/data-display/data-display.component.ts
@@ -14,6 +14,7 @@ let cpuGraph;
 export class DataDisplayComponent implements OnInit {
 
     constructor(private fusionbot: FusionbotService) {
+
     }
 
     public options: Object;
@@ -21,40 +22,102 @@ export class DataDisplayComponent implements OnInit {
 
     ngOnInit() {
         this.fusionbot.getData().subscribe((data) => {
-            const graphData = data.data.map((d, k) => {
-                return [k, d];
-            });
-            const emaGraphData = data.ema.map((d, k) => {
-                return [k, parseFloat(d)];
-            });
-            const emsRanges = data.allEMS.map((d) => {
-                return d;
-            });
-            console.log(emaGraphData);
-           // document.addEventListener('DOMContentLoaded', function () {
+                const graphData = data.data.map((d, k) => {
+                    return [k, d];
+                });
+                const emaGraphData = data.ema.map((d, k) => {
+                    return [k, parseFloat(d)];
+                });
+                const emsRanges = data.allEMS.map((d) => {
+                    return d;
+                });
+                console.log(emaGraphData);
                 cpuGraph = Highcharts.chart('cpuGraph', {
-                    title: {
-                        text: 'CPU Data'
+                    colors: ['#a47cff',
+                        '#6300ff',
+                        '#ffffff'
+                    ],
+                    chart: {
+                        backgroundColor: {
+                            linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                            stops: [
+                                [0, '#2f2e4c'],
+                                [1, 'rgb(28,32,41)']
+                            ]
+                        }
                     },
-                    // data: {
-                    //     enablePolling: true,
-                    //     dataRefreshRate: 1
-                    // },
+                    title: {
+                        text: 'CPU Temperature',
+                        style: {
+                            color: '#FFF',
+                            fontWeight: 'bold'
+                        }
+                    },
+                    yAxis: {
+                        min: 0.0,
+                        max: 100,
+                        title: {
+                            text: 'Usage Percentage',
+                            style: {
+                                color: '#CCC',
+                            }
+                        },
+                        labels: {
+                            style: {
+                                color: '#CCC'
+                            }
+                        }
+                    },
+                    xAxis: {
+                        title: {
+                            text: 'Tick',
+                            style: {
+                                color: '#CCC',
+                            }
+                        },
+                        labels: {
+                            style: {
+                                color: '#CCC',
+                            }
+                        }
+                    },
+                    legend: {
+                        itemStyle: {
+                            color: '#CCC'
+                        },
+                        itemHoverStyle: {
+                            color: '#892dff'
+                        },
+                        itemHiddenStyle: {
+                            color: '#555'
+                        }
+                    },
+                    labels: {
+                        style: {
+                            color: '#CCC',
+                        }
+                    },
+                    tooltip: {
+                        crosshairs: true,
+                        shared: true,
+                        valueSuffix: '%'
+                    },
                     series: [{
-                        name: 'data',
-                        data: graphData,
-                    }, {
-                        name: 'EMS Range',
+                        name: 'AI Error Margin',
                         data: emsRanges,
                         type: 'arearange'
+
                     }, {
-                        name: 'EMA',
+                        name: 'AI Prediction',
                         data: emaGraphData
+                    }, {
+                        name: 'CPU Utilization',
+                        data: graphData,
                     }]
                 });
 
                 this.loaded = true;
-            });
-     //   });
+            }
+        );
     }
 }
